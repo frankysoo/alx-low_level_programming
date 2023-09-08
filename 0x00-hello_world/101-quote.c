5bcfb94adc24e60a46af84c897a42c1063aaa711
moad <apkmouad@gmail.com>
2023-09-07 19:23:25 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ *  * main - Entry point
+ *   * Return: Always 0 (Success)
+ *   */
+int main(void)
+{
+		char mes[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+			write(2, mes, strlen(mes));
+				return (1);
+}
