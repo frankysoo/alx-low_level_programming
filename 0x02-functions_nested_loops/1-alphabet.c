f272e4b04d49eb5eb39cc323adb81073d31f67cd
moad <apkmouad@gmail.com>
2023-09-13 12:17:35 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,9 @@
 * Description: Print @c c_putchar.
 * Return:0
 */
-void print_alphabet(void);
-{
+void print_alphabet(void){
 char c ;
 for (c = 'a' ; c <= 'z' ; c++){
-{
 _putchar(c);
 }
 _putchar('\n');
