fb1aed00c415c0c9031ad800ca5dbc8d09f314de
moad <apkmouad@gmail.com>
2023-09-13 11:54:21 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Print @c c_putchar.
+ * Return:0
+ */
+void print_alphabet(void);
+{
+char c ;
+for(c = 'a' ; c <= 'z' ; c++){
+{
+_putchar(c);
+}
+_putchar('\n');
+}
