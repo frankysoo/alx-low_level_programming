734620f9cfc353a535984fe322d06785eaf1127f
moad <apkmouad@gmail.com>
2023-09-13 12:14:03 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Print @c c_putchar.
+ * Return:0
+ */
+
+void print_alphabet_x10(void){
+char c ;
+int i ;
+for(i = 0 ; i < 10 ; i++){
+for(c = 'a' ; c <= 'z' ; c++){
+putchar(c);}
+putchar('\n');}
+}
