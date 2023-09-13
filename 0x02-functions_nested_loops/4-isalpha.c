9788915fc13fe9431cb22c96d9bbf223864f52c3
moad <apkmouad@gmail.com>
2023-09-13 12:56:19 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/**
-* _islower - Return 1 if letter is lowercase or upercase, 0 if not.
-* @c: char type letter
-* Return: 1 if lowercase or upercase, 0 if not lowercase
-*/
+ /**
+ * _isalpha - Check if character is an alphabetic character.
+ * @c: type int character
+ * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
+ */
 int _islower(int c){
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return 1 ;
