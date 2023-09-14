711e2eb2e5845073b96b03213ce0272164b48081
moad <apkmouad@gmail.com>
2023-09-14 14:54:49 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*	* print_numbers - Prints numbers
+*	* Return:0
+*	*/
+void print_numbers(void)
+{
+char i ;
+for (i = '0' ; i <= '9' ; i++)
+{
+putchar(i) ;
+}
+putchar('\n') ;
+}
