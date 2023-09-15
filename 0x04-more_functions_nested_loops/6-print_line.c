ba2dab2251edbf168db8bc6a393a1e284efd8217
moad <apkmouad@gmail.com>
2023-09-14 17:54:26 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
-*	* print_line - Check for a digit
-*	* @n : number of _ to be printed
-*	* Return:void
-*	*/
+ *  * print_line - Check for a digit
+ *   * @n : number of _ to be printed
+ *    * Return:void
+ *     */
 
 void print_line(int n)
 {
-int i ;
-for (i = 0 ; i <= n ; i++)
-{
-putchar('_');
-if (n == 0)
-_putchar('\n');
-}
-_putchar('\n');
-}
 
+		int i = 0;
+
+			while (i < n && n > 0)
+					{
+								_putchar('_');
+										i++;
+											}
+				_putchar('\n');
+
+}
