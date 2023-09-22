138972b14049185afb8298a3bb797827e3cc74ec
moad <apkmouad@gmail.com>
2023-09-21 19:59:21 -0700

    More Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
+/*
 *	* print_buffer - Prints buffer
 *	* @b: buffer
 *	* @size: size
 *	* Return: void
-*	*/
+*/
 
 void print_buffer(char *b, int size)
 {
