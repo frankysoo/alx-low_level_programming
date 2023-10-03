3a8fac3626ebad13fc08f960d19f485ec2b0f83d
moad <apkmouad@gmail.com>
2023-10-02 19:51:18 -0700

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*	* main - function that prints a string, followed by a new line
+*	* @argv: The string to print
+*	* @argc: The string to print
+*	*
+*	* Return: nothing.
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*	* main - function that prints a string, followed by a new line
+*	* @argv: The string to print
+*	* @argc: The string to print
+*	*
+*	* Return: nothing.
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
