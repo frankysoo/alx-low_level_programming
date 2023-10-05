99529177d8cb8fc9b26e7ecfc890154d27add5b9
moad <apkmouad@gmail.com>
2023-10-04 19:36:48 -0700

    malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*	* alloc_grid - function that prints a string, followed by a new line
+*	* @width: The string to print
+*	* @height: The string to print
+*	*
+*	* Return: nothing.
+*/
+int **alloc_grid(int width, int height)
+{
+int **s;
+int i, j;
+
+if (height <= 0 || width <= 0)
+return (NULL);
+s = malloc(sizeof(int *) * height);
+if (s == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+s[i] = malloc(sizeof(int) * width);
+
+if (s[i] == NULL)
+{
+for (; i >= 0; i--)
+free(s[i]);
+
+free(s);
+return (NULL);
+}
